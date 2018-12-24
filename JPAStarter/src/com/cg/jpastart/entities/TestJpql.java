646fa9d4c189ef1dd149b389110d903dfbb3cf6e
Divyadevi56 <45747762+Divyadevi56@users.noreply.github.com>
2018-12-24 04:31:40 -0800

    Add files via upload

---
+++ JPAStarter/src/com/cg/jpastart/entities/TestJpql.java
@@ -0,0 +1,10 @@
+package com.cg.jpastart.entities;
+
+import javax.persistence.EntityManagerFactory;
+
+public class TestJpql {
+public static void main(String[] args) {
+	EntityManagerFactory factory=Persistance.EntityManagerFactory("");
+	
+}
+}
